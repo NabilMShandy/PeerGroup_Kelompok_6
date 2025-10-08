48b9ea9183217d7bd7731248baa58b2c20ae64f7
Nabil Muhammad Shandy <nmuhammadshandy@gmail.com>
2025-10-08 15:47:42 +0700

    Add files via upload

---
+++ projectpeergroupnabil.cpp
@@ -0,0 +1,27 @@
+// Fungsi utama main untuk menjalankan program
+int main()
+{
+    int pilihan;
+    int jumlah;
+
+    // Menggunakan loop do while supaya menjalankan program setidaknya sekali sebelum loop berjalan
+    do
+    {
+        menu(); 
+        cout << "masukkan pilihan: ";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+            cout << "Masukkan jumlah yang ingin didepositkan: ";
+            cin >> jumlah;
+            
+            if(jumlah < 0){
+                cout << "Deposit tidak boleh negatif!\n";
+                break;
+            }
+
+            // Menyimpan nilai dalam fungsi deposit ke variabel saldo
+            saldo = deposit(saldo, jumlah);
+            break;
