e4e684d6ad698dc77b04da4ea60a21d84ad18e74
pisangambon-suroboyo <galanggemilang004@gmail.com>
2025-10-08 15:51:02 +0700

    Add files via upload

---
+++ peergalang.cpp
@@ -0,0 +1,7 @@
+// 2. Dalam menu deposito, menggunakan function yang mengembalikan nilai supaya nilai sebelumnya disimpan dan dapat dijumlahkan dengan nilai baru
+int deposit(int saldof, int jumlahf)
+{
+    int saldo_baru = saldof + jumlahf;
+    cout << "Anda melakukan deposit sebesar" << " Rp" << saldo_baru << endl;
+    return saldo_baru;
+}
