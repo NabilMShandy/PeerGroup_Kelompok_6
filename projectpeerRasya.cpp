13604b073f176dcde796f6461fc2b183311cb2b5
RasyaFFFFF <rasyafadilah0311@gmail.com>
2025-10-09 19:32:19 +0700

    Add files via upload

---
+++ projectpeerRasya.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// Deklarasi variabel untuk dipanggil
+int saldo;
+int tarikk = 0;
+
+void menu()
+{
+    cout << "================================\n";
+    cout << "PROGRAM SEDERHANA MENYIMPAN UANG\n";
+    cout << "================================";
+    cout << "\n";
+    cout << "Pilih menu yang ingin digunakan\n";
+    cout << "1. DEPOSTIO UANG\n";
+    cout << "2. TARIK TUNAI\n";
+    cout << "3. CEK SALDO\n";
+    cout << "4. KELUAR PROGRAM\n";
+}
